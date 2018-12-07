ae018a6789d6efa69f993e1a8608a3000ca0064a
Matthew Boehlig <thetamind@gmail.com>
2018-12-07 02:57:33 -0600

    Day 3 parse into claim struct

--- day03.exs
+++ day03.exs
@@ -1,4 +1,16 @@
 defmodule Day03 do
+  defmodule Claim do
+    defstruct [:id, :left, :top, :width, :height]
+
+    @type t :: %Claim{
+            id: non_neg_integer,
+            left: non_neg_integer,
+            top: non_neg_integer,
+            width: non_neg_integer,
+            height: non_neg_integer
+          }
+  end
+
   def parse_input(lines) do
     lines
     |> Enum.map(&parse_claim/1)
@@ -7,21 +19,11 @@ defmodule Day03 do
   def parse_claim(input) do
     regex = ~r/#(?<id>\d+) @ (?<left>\d+),(?<top>\d+): (?<width>\d+)x(?<height>\d+)/
 
-    c =
+    captures =
       Regex.named_captures(regex, input)
-      |> Enum.into(%{}, fn {k, v} -> {k, String.to_integer(v)} end)
+      |> Enum.into(%{}, fn {k, v} -> {String.to_existing_atom(k), String.to_integer(v)} end)
 
-    {
-      c["id"],
-      {
-        c["left"],
-        c["top"]
-      },
-      {
-        c["width"],
-        c["height"]
-      }
-    }
+    Map.merge(%Claim{}, captures)
   end
 end
 
@@ -30,11 +32,14 @@ ExUnit.start(trace: true, seed: 0)
 defmodule Day03Test do
   use ExUnit.Case
 
+  alias Day03.Claim
+
   describe "part 1" do
     test "parse input to claims" do
       claims = Day03.parse_input(example_input())
+      claim = Enum.at(claims, 0)
 
-      assert {1, {1, 3}, {4, 4}} = Enum.at(claims, 0)
+      assert %Claim{id: 1, left: 1, top: 3, width: 4, height: 4} = claim
     end
 
     defp example_input() do
