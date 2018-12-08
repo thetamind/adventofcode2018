977da757a20a08790cca813e9469532a1653a738
Matthew Boehlig <thetamind@gmail.com>
2018-12-08 00:26:07 -0600

    Day 3 solution

--- day03.exs
+++ day03.exs
@@ -9,6 +9,52 @@ defmodule Day03 do
             width: non_neg_integer,
             height: non_neg_integer
           }
+
+    def squares(%Claim{left: left, top: top, width: width, height: height}) do
+      right = left + width - 1
+      bottom = top + height - 1
+
+      for y <- top..bottom, x <- left..right, do: {x, y}
+    end
+  end
+
+  defmodule Fabric do
+    defstruct allocations: %{}
+
+    @opaque t :: %Fabric{
+              allocations: map
+            }
+
+    def add_claim(%Fabric{allocations: allocations} = fabric, claim) do
+      allocations =
+        claim
+        |> Claim.squares()
+        |> Enum.reduce(allocations, fn key, acc ->
+          Map.update(acc, key, [claim.id], &(&1 ++ [claim.id]))
+        end)
+
+      %{fabric | :allocations => allocations}
+    end
+
+    def at(%Fabric{} = fabric, {x, y}), do: at(fabric, x, y)
+
+    def at(%Fabric{} = fabric, x, y) do
+      Map.get(fabric.allocations, {x, y}, [])
+    end
+
+    def new(claims) do
+      claims
+      |> Enum.reduce(%Fabric{}, &reducer/2)
+    end
+
+    defp reducer(claim, acc) do
+      Fabric.add_claim(acc, claim)
+    end
+
+    def overlapping_count(%Fabric{allocations: allocations}) do
+      allocations
+      |> Enum.count(fn {_pos, ids} -> Enum.count(ids) > 1 end)
+    end
   end
 
   def parse_input(lines) do
@@ -30,16 +76,51 @@ end
 ExUnit.start(trace: true, seed: 0)
 
 defmodule Day03Test do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  alias Day03.Claim
+  alias Day03.{Claim, Fabric}
 
-  describe "part 1" do
+  describe "claim" do
+    test "squares" do
+      claim = %Claim{id: 1, left: 0, top: 0, width: 2, height: 3}
+      expected = [{0, 0}, {1, 0}, {0, 1}, {1, 1}, {0, 2}, {1, 2}]
+      assert expected == Claim.squares(claim)
+    end
+  end
+
+  describe "fabric" do
+    test "allocate squares" do
+      claim1 = %Claim{id: 1, left: 1, top: 3, width: 4, height: 4}
+      claim2 = %Claim{id: 2, left: 3, top: 1, width: 4, height: 4}
+
+      fabric = %Fabric{}
+      fabric = Fabric.add_claim(fabric, claim1)
+      assert [1] == Fabric.at(fabric, 1, 3)
+      assert [1] == Fabric.at(fabric, 3, 3)
+
+      fabric = Fabric.add_claim(fabric, claim2)
+      assert [1, 2] == Fabric.at(fabric, 3, 3)
+    end
+  end
+
+  describe "day 3" do
     test "parse input to claims" do
       claims = Day03.parse_input(example_input())
       claim = Enum.at(claims, 0)
 
       assert %Claim{id: 1, left: 1, top: 3, width: 4, height: 4} = claim
+    end
+
+    test "example overlapping count" do
+      claims = Day03.parse_input(example_input())
+      fabric = Fabric.new(claims)
+      assert 4 == Fabric.overlapping_count(fabric)
+    end
+
+    test "puzzle overlapping count" do
+      claims = Day03.parse_input(puzzle_input())
+      fabric = Fabric.new(claims)
+      assert 109716 == Fabric.overlapping_count(fabric)
     end
 
     defp example_input() do
