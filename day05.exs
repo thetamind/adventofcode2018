0a5c2eca739955c032337461b3a029e5731d1e9e
Matthew Boehlig <thetamind@gmail.com>
2018-12-15 12:39:16 -0600

    Day 5b solution

--- day05.exs
+++ day05.exs
@@ -14,6 +14,27 @@ defmodule Day5 do
   def do_shrink("", acc) do
     String.reverse(acc)
   end
+
+  def part2(polymer) do
+    choices(polymer)
+    |> Enum.sort_by(fn {_, length, _} -> length end, &<=/2)
+    |> Enum.at(0)
+  end
+
+  @patterns Enum.map(?a..?z, fn unit ->
+              pattern = ~r/[#{<<unit>>}|#{String.upcase(<<unit>>)}]/
+              {<<unit>>, pattern}
+            end)
+
+  def choices(polymer) do
+    Enum.map(@patterns, fn {unit, pattern} ->
+      shrunk = String.replace(polymer, pattern, "") |> shrink()
+
+      length = String.length(shrunk)
+
+      {unit, length, shrunk}
+    end)
+  end
 end
 
 ExUnit.start(seed: 0, trace: true)
@@ -45,10 +66,21 @@ defmodule Day5Test do
     end
   end
 
+  describe "part 2 example" do
+    test "determine which type to remove to produce shortest polymer" do
+      assert {"c", 4, "daDA"} == Day5.part2("dabAcCaCBAcCcaDA")
+    end
+  end
+
   describe "puzzle" do
     test "polymer reaction shrinks matching pairs" do
       input = File.read!("day05.txt") |> String.trim_trailing("\n")
       assert 11720 == Day5.shrink(input) |> String.length()
+    end
+
+    test "determine which type to remove to produce shortest polymer" do
+      input = File.read!("day05.txt") |> String.trim_trailing("\n")
+      assert {"w", 4956, _} = Day5.part2(input)
     end
   end
 end
