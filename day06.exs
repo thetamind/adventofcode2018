ba51a5b8324528edb1e5329f43895f21ecc73b64
Matthew Boehlig <thetamind@gmail.com>
2018-12-22 12:47:09 -0600

    Day 6 rework/debug grid labelling

--- day06.exs
+++ day06.exs
@@ -28,6 +28,16 @@ defmodule Day6 do
   defp first_last(plot) do
     List.first(plot) ++ List.last(plot)
   end
+
+  def manhattan_sum(lookup, point) do
+    Map.fetch!(lookup, point)
+  end
+
+  def safe_region_size(lookup, threshold) do
+    lookup
+    |> Enum.filter(fn {_point, sum} -> sum < threshold end)
+    |> Enum.count()
+  end
 end
 
 defmodule Day6.Grid do
@@ -62,6 +72,101 @@ defmodule Day6.Grid do
         end
       end
     end
+  end
+
+  def manhattan_sums(coordinates, size \\ 9) do
+    for y <- 0..size do
+      for x <- 0..size do
+        point = {x, y}
+
+        {point, manhattan_sum(coordinates, point)}
+      end
+    end
+  end
+
+  def manhattan_sum(coordinates, point) do
+    coordinates
+    |> Enum.map(&manhattan(&1, point))
+    |> Enum.sum()
+  end
+
+  def to_lookup(grid) do
+    grid
+    |> List.flatten()
+    |> Map.new()
+  end
+
+  def label_grid(grid, labeller) do
+    grid
+    |> Enum.map(&label_row(&1, labeller))
+  end
+
+  def label_row(row, labeller) do
+    row
+    |> Enum.map(fn {point, value} ->
+      labeller.(point, value)
+    end)
+  end
+
+  def ascii_labeller(values) do
+    # pixels = String.split(" .,:;i1tfLCG08@", "", trim: true)
+    pixels = String.split(" .○☼•❄", "", trim: true)
+    pixel_count = Enum.count(pixels)
+    bins = make_bins(values, pixels)
+    labeller = make_labeller(bins, pixels)
+
+    pixel_for = fn value ->
+      Enum.find(labeller, {:not_found, "?"}, fn {{low, high}, label} ->
+        value >= low and value <= high
+      end)
+      |> elem(1)
+    end
+
+    fn _point, value ->
+      pixel_for.(value)
+    end
+  end
+
+  def make_labeller(bins, labels) do
+    bins
+    |> Enum.zip(labels)
+    |> IO.inspect(label: "bins")
+  end
+
+  def make_bins(values, labels) do
+    IO.puts(labels |> Enum.join())
+    count = Enum.count(labels)
+    values = Enum.sort(values)
+    {min, max} = Enum.min_max(values)
+    IO.inspect({min, max}, label: "range")
+    rise = max - min
+    run = Enum.count(values)
+    IO.inspect(rise, label: "rise")
+    IO.inspect(run, label: "run")
+
+    slope =
+      (rise / run)
+      |> IO.inspect(label: "slope")
+
+    # y - y1 = m(x - x1)
+    x = 20
+    y = slope * x + min
+    IO.inspect(y, label: "y")
+
+    uniq_values = Enum.uniq(values) |> Enum.sort()
+    uniq_count = Enum.count(uniq_values)
+
+    chunk_size =
+      max(div(uniq_count, count), 1)
+      |> IO.inspect(label: "chunk_size")
+
+    keys =
+      uniq_values
+      |> Enum.chunk_every(chunk_size)
+      |> Enum.map(fn chunk ->
+        {List.first(chunk), List.last(chunk)}
+      end)
+      |> IO.inspect(label: "chunks", charlists: :as_lists)
   end
 
   def inspect(plot) do
@@ -182,8 +287,8 @@ defmodule Day6Test do
     end
   end
 
-  describe "largest area" do
-    test "example" do
+  describe "example" do
+    test "largest area" do
       coordinates = [
         {1, 1},
         {1, 6},
@@ -194,6 +299,35 @@ defmodule Day6Test do
       ]
 
       assert {"E", 17} = Day6.largest_area(coordinates)
+    end
+
+    test "manhattan sum" do
+      coordinates = [
+        {1, 1},
+        {1, 6},
+        {8, 3},
+        {3, 4},
+        {5, 5},
+        {8, 9}
+      ]
+
+      grid = Day6.Grid.manhattan_sums(coordinates)
+      lookup = Day6.Grid.to_lookup(grid)
+
+      assert 30 = Day6.manhattan_sum(lookup, {4, 3})
+      # assert 16 = Day6.safe_region_size(lookup, 32)
+
+      IO.puts("\n")
+
+      labeller =
+        lookup
+        |> Map.values()
+        |> Day6.Grid.ascii_labeller()
+
+      grid
+      |> Day6.Grid.label_grid(labeller)
+      |> Day6.Grid.inspect()
+      |> IO.puts()
     end
   end
 
@@ -211,6 +345,17 @@ defmodule Day6Test do
 
       assert {"i", 3840} = Day6.largest_area(coordinates, size)
     end
+
+    test "manhattan sum" do
+      coordinates =
+        File.read!("day06.txt")
+        |> String.split("\n", trim: true)
+        |> Enum.map(&parse_coordinate/1)
+
+      grid = Day6.Grid.manhattan_sums(coordinates)
+      lookup = Day6.Grid.to_lookup(grid)
+      assert 16 == Day6.safe_region_size(lookup, 10_000)
+    end
   end
 
   def parse_coordinate(string) do
@@ -218,5 +363,32 @@ defmodule Day6Test do
     |> String.split(", ")
     |> Enum.map(&String.to_integer/1)
     |> List.to_tuple()
+  end
+end
+
+defmodule Day6.BinTest do
+  use ExUnit.Case, async: true
+
+  test "lookups" do
+    lookup = [
+      {{28, 30}, " "},
+      {{32, 34}, "."},
+      {{36, 38}, "○"},
+      {{40, 42}, "☼"},
+      {{44, 46}, "•"},
+      {{48, 50}, "❄"}
+    ]
+
+    pixel_for = fn value ->
+      Enum.find(lookup, {:not_found, "?"}, fn {{low, high}, label} ->
+        value >= low and value <= high
+      end)
+    end
+
+    assert {{32, 34}, "."} = pixel_for.(32)
+    assert {{32, 34}, "."} = pixel_for.(33)
+    assert {{32, 34}, "."} = pixel_for.(34)
+    assert {{36, 38}, "○"} = pixel_for.(36)
+    assert {:not_found, "?"} = pixel_for.(99)
   end
 end
