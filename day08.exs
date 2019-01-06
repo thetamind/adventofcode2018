70e70e9557f26e684de0f6c9ca76fb4c2f2495f7
Matthew Boehlig <thetamind@gmail.com>
2019-01-05 18:01:17 -0600

    Day 8 tree module

--- day08.exs
+++ day08.exs
@@ -1,4 +1,39 @@
+defmodule Day8.Node do
+  defstruct children: [], meta: []
+
+  def new(children, meta) when is_list(children) and is_list(meta) do
+    %__MODULE__{children: children, meta: meta}
+  end
+
+  def child(tree, index) do
+    Enum.at(tree.children, index)
+  end
+
+  def meta(tree) do
+    tree.meta
+  end
+
+  def tree_size([]), do: 1
+
+  def tree_size(%__MODULE__{children: children}) do
+    Enum.reduce(children, 1, fn child, acc ->
+      acc + tree_size(child)
+    end)
+  end
+
+  def meta_maximum(%__MODULE__{children: children, meta: meta}) do
+    max_children =
+      Enum.reduce(children, 0, fn child, acc ->
+        max(acc, meta_maximum(child))
+      end)
+
+    max(max_children, Enum.max(meta))
+  end
+end
+
 defmodule Day8 do
+  alias Day8.Node
+
   def parse(input) do
     input
     |> String.split()
@@ -6,17 +41,64 @@ defmodule Day8 do
   end
 
   def to_tree(data) do
+    tree(data)
+  end
 
+  def tree([child_count | [meta_count | tail]]) do
+    children = 0..child_count |> Enum.map(fn i -> Node.new([], [i]) end)
+    meta = 0..meta_count |> Enum.to_list()
+    Node.new(children, meta)
   end
 
   def meta_sum(tree) do
-    0
+    tree
   end
 end
 
 ExUnit.start(seed: 0, trace: true)
 
-defmodule Day08Test do
+defmodule Day8.NodeTest do
+  use ExUnit.Case, async: true
+
+  alias Day8.Node
+
+  describe "new" do
+    test "children" do
+      meta =
+        sample_tree()
+        |> Node.child(1)
+        |> Node.child(0)
+        |> Node.meta()
+
+      assert [99] == meta
+    end
+
+    test "tree_size" do
+      assert 4 == Node.tree_size(sample_tree())
+    end
+
+    test "meta_maximum" do
+      assert 99 == Node.meta_maximum(sample_tree())
+    end
+  end
+
+  def sample_tree do
+    Node.new(
+      [
+        Node.new([], [10, 11, 12]),
+        Node.new(
+          [
+            Node.new([], [99])
+          ],
+          [2]
+        )
+      ],
+      [1, 1, 2]
+    )
+  end
+end
+
+defmodule Day8Test do
   use ExUnit.Case, async: true
 
   describe "part 1" do
